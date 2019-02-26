53a255ea8a8b0257d4e3e53e539bdc3b827f9cd1
Christian Trinh <trinhc2@mcmaster.ca>
2019-02-26 22:44:30 +0000

    Added my custom feature, added a case where the user's input is invalid and updated the readme

--- Project01/README.md
+++ Project01/README.md
@@ -2,9 +2,13 @@
 
 I selected the following features:
 * File Type Count
+  * Returns a file count for how many file types exist.
 * TODO Log
+  * Puts every line with a TODO tag into the file "todo.log"
 * Delete Temporary Files
+  * Deletes all untracked files with the .tmp extension
 * Merge Log
+  * Finds the commit hashes where merge is mentioned in the commit message and puts it into the file "merge.log"
 
 ### Instructions
 
@@ -15,3 +19,16 @@ I selected the following features:
 4. Enter the number of the function and hit "Enter".
 5. The script will run without further input needed.
 
+### Custom Feature (Size of Folder/File)
+
+My custom feature finds the size of a folder or file.
+If the user wants the size of a file, it will return the size of the file.
+If the user wants to find the size of a folder, it will return the folder size as well as the largest object in folder.
+
+To use my custom feature:
+
+1. Run the script
+2. Enter the number "5"
+3. The script will list everything in the current directory
+4. Enter the name of the file/folder
+5. The script will run by itself
--- Project01/project_analyze.sh
+++ Project01/project_analyze.sh
@@ -7,6 +7,7 @@ echo "Which functions which you like to execute:
 	2) TODO log
 	3) Delete Temporary Files
 	4) Merge Log
+	5) Size of Folder/File
 Enter a Number followed by the Enter key."
 
 read funcNum
@@ -42,7 +43,53 @@ git ls-files . --exclude-standard --others | find . -name "*.tmp" | xargs rm
 
 mergeLog () {
 
+#looks for the line with merge and adds the has to merge.log
 git log --oneline | grep -i "merge" | cut -d ' ' -f1 > merge.log
+
+}
+
+sizeOfF () {
+
+echo "What would you like to find the size of?"
+
+#outputs the names of the files and folders in the current directory
+ls -1
+
+echo "Enter the name of the file/folder (case sensitive) followed by the Enter key"
+read fname
+
+#if a file is inputted
+if [ -f $fname ]; then
+	#stores the first column in a variable (the first row contains the size of the folder/file)
+	fsize=$(du -h "$fname" | cut -f1)
+	echo "The size of $fname is $fsize"
+
+#if a directory is inputted
+elif [ -d $fname ]; then
+	fsize=$(du -h "$fname" | cut -f1)
+	echo "the size of $fname is $fsize"
+	cd $fname
+
+	#finds the size of everything in the directory, sorts the first column, takes the 2nd last entry because the last entry is the sub-directory itself
+	largestFileSize=$(du -ah | sort -k1n | cut -f1 | tail -2 | head -1)
+
+	#same as before except we cut the second column to get the names of everything, we cut the first 2 letters because they start with ./
+	largestFName=$(du -ah | sort -k1n | cut -f2 | tail -2 | head -1 | cut -c3-)
+
+	#if the object in the directory is a file
+	if [ -f $largestFileName ]; then
+		echo "The largest object in $fname is the file $largestFName ($largestFileSize)"
+
+	#if the object in the directory is a directory
+	elif [ -d $largestFileName ]; then
+		echo "The largest object in $fname is the folder $largestFName ($largestFileSize)"
+
+	fi
+
+else
+	echo "Invalid input"
+
+fi
 
 }
 
@@ -61,6 +108,12 @@ elif [ $funcNum = "3" ]; then
 elif [ $funcNum = "4" ]; then
 	echo "Creating Merge Log"
 	mergeLog
+
+elif [ $funcNum = "5" ]; then
+	sizeOfF
+
+else
+	echo "Invalid input"
 
 fi
 
