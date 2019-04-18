39077202912658b94e5909fda93f80d121b18ea3
Christian Trinh <trinhc2@mcmaster.ca>
2019-04-18 15:12:24 -0400

    Added basic game states and changed the catcher to a cup, decided to make the game about filling up a cup with rain water

--- Project03/catch.elm
+++ Project03/catch.elm
@@ -10,6 +10,8 @@ type Msg = Tick Float GetKeyState
          | MakeRequest Browser.UrlRequest
          | UrlChange Url.Url
          | CreateNewX Float
+         | StartGame
+         | RestartGame
 
 type alias Model = { health : Float
                    , score : Float
@@ -17,8 +19,10 @@ type alias Model = { health : Float
                    , y : Float
                    , rainY : Float
                    , rainX : Float
+                   , gameState : State
                    }
 
+type State = Start | Active | Finish
 randomX : Random.Generator Float
 randomX =
   Random.float -200 200
@@ -30,49 +34,67 @@ generateRandomX =
 init : () -> Url.Url -> Key -> ( Model, Cmd Msg )
 init flags url key =
   let
-    model = { health = 3, score = 0, x = 0, y = 0, rainY = 200, rainX = 0}
+    model = { health = 3, score = 0, x = 0, y = 0, rainY = 200, rainX = 0, gameState = Start}
   in ( model , Cmd.none ) -- add init model
 
 update : Msg -> Model -> ( Model, Cmd Msg )
-update msg model = case msg of
+update msg model = if model.gameState == Active then
+                    case msg of
                      Tick time (keyToState, _, (x,y)) ->
                        let
                         boostx = case keyToState (Key "j") of
                           Down -> 3
                           _ -> 1
                        in
-                        if model.rainY < -250
-                          then ({ model | rainY = 250
-                                        , health = model.health - 1}, generateRandomX)
-                        else
+                          if model.rainY < -250
+                              then ({ model | rainY = 250
+                                            , health = model.health - 1}, generateRandomX)
+                          else
+                              if (model.rainX - model.x < 40 && model.rainX - model.x > -40)  && ((model.rainY - 7.5) - (model.y - 130) < 10 && (model.rainY - 7.5) - (model.y - 130) > -10)
+                                  then ({ model | rainY = 250
+                                                , score = model.score + 1}, generateRandomX)
+                              else
+                                if model.health == 0 then ({ model | gameState = Finish}, Cmd.none) else
+                                  ({model | x = if model.x <= -210 then model.x + 1 else if model.x >= 210 then model.x - 1 else model.x + 4*x*boostx
+                                          , rainY = model.rainY - 9
+                                  }, Cmd.none)
 
-                         if (model.rainX - model.x < 40 && model.rainX - model.x > -40)  && ((model.rainY - 7.5) - (model.y - 130) < 10)
-                           then ({ model | rainY = 250
-                                , score = model.score + 1}, generateRandomX)
-
-                         else
-                          ({model | x = if model.x <= -210 then model.x + 1 else if model.x >= 210 then model.x - 1 else model.x + 4*x*boostx
-                                  , rainY = model.rainY - 9
-                          }, Cmd.none)
-
---                     Tick time keystate ->
-  --                     let
-    --                    newpositiony = model.positiony + model.vy
-      --                 in ( { model | positiony = newpositiony }, Cmd.none )
                      MakeRequest req    -> ( model , Cmd.none )
                      UrlChange url      -> ( model , Cmd.none )
+                     StartGame -> ( model , Cmd.none)
                      CreateNewX newrainx -> ({model | rainX = newrainx}, Cmd.none)
+                     RestartGame -> ( model , Cmd.none)
+
+                    else if model.gameState == Finish then
+                      case msg of
+                        StartGame -> ( model , Cmd.none)
+                        MakeRequest _    -> ( model , Cmd.none )
+                        UrlChange _      -> ( model , Cmd.none )
+                        Tick _ _ -> ( model , Cmd.none )
+                        CreateNewX _ -> ( model , Cmd.none )
+                        RestartGame -> ( {model | health = 3, score = 0, x = 0, y = 0, rainY = 200, rainX = 0, gameState = Start} , Cmd.none)
+                        else
+                          case msg of
+                            StartGame -> ( {model | gameState = Active} , Cmd.none)
+                            MakeRequest _    -> ( model , Cmd.none )
+                            UrlChange _      -> ( model , Cmd.none )
+                            Tick _ _ -> ( model , Cmd.none )
+                            CreateNewX _ -> ( model , Cmd.none )
+                            RestartGame -> ( model , Cmd.none)
+
 view : Model -> { title : String, body : Collage Msg }
 view model =
   let
-    title = "Missile Command"
+    title = "Fill up my cup"
     body = collage 500 375 shapes
-    shapes = [ assets, rainDrop, ground, catcher |> move (model.x, model.y) ]
+    shapes = [ assets, rainDrop, ground, catcher |> move (model.x, model.y)]
     assets = group [ background, currentHealth, currentScore]
-    catcher = group [ stick, plate]
+    catcher = group [ cup, handle, innerhandle]
 
     background = rectangle 500 375
                   |> filled black
+                  |> notifyTap StartGame
+                  |> notifyTap RestartGame
 
     currentHealth = text ("Health: " ++ String.fromFloat model.health)
                       |>filled white
@@ -86,14 +108,19 @@ view model =
              |> filled green
              |> move (0,-180)
 
-    stick = rectangle 4 40
-                |> filled red
-                |> move (0,-150)
-
-    plate = curve (-40,0) [Pull (0,-30) (40,0)]
+    cup = rectangle 75 50
               |> filled red
-              |> move (0,-120)
+              |> move (0, -145)
 
+    handle = curve (0,0) [Pull (40,-20) (0,-40)]
+              |>filled red
+              |>move (35,-125)
+
+    innerhandle = curve (0,0) [Pull (40,-20) (0,-40)]
+              |>filled black
+              |>scale 0.5
+              |>move (40,-135)
+              
     rainDrop = circle 15
                 |> filled blue
                 |> move (model.rainX,model.rainY)
